9e10682b8238e2a8f1cdf8323689c0d56638b35c
张琳函 <duanduan@zhanglinhandeMacBook.local>
2018-11-05 22:44:46 +0800

    3-3

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -15,6 +15,16 @@ Page({
 
   },
 
+  onGetUserInfo: function (e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -26,7 +36,29 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
 
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -6,10 +6,6 @@ Page({
    */
   data: {
     userInfo: null,
-    // userInfo: {
-    //   nickName: "优达学城",
-    //   avatarUrl: "", // 头像 URL 地址
-    // }, // 虚拟数据
   },
 
   onTapAddress() {
@@ -29,29 +25,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
+    
   },
 
   onGetUserInfo: function (e) {
@@ -75,8 +49,30 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
 
-  },
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },  
 
   /**
    * 生命周期函数--监听页面隐藏
